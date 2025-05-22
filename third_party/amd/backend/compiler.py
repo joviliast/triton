6c36ed180d05f25354de628d18a897da590d85e6
Alexander Efimov <efimov.alexander@gmail.com>
2025-05-22 20:12:02 +0000

    do not call coalesce pass if aggregate load is disabled

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -225,9 +225,10 @@ class HIPBackend(BaseBackend):
         amd.passes.ttgpuir.add_accelerate_matmul(pm, options.arch, options.matrix_instr_nonkdim, options.kpack)
         passes.ttgpuir.add_remove_layout_conversions(pm)
         amd.passes.ttgpuir.add_optimize_epilogue(pm)
-        amd.passes.ttgpuir.add_aggregate_load(pm, options.arch, options.aggregate_load_factor)
 
-        passes.ttgpuir.add_coalesce(pm)
+        if options.aggregate_load_factor == -1 or options.aggregate_load_factor > 1:
+            amd.passes.ttgpuir.add_aggregate_load(pm, options.arch, options.aggregate_load_factor)
+            passes.ttgpuir.add_coalesce(pm)
 
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
         amd.passes.ttgpuir.add_hoist_layout_conversions(pm)
